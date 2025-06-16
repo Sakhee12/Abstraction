4031747949be7a02829f5217176903ffd35771ee
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-06-16 22:53:15 +0530

    Add files via upload

---
+++ Specifier.java
@@ -0,0 +1,34 @@
+package PrinciplesOfOops;
+
+public abstract class Specifier 
+{
+	double pi = 3.14;
+	public abstract void areaOfCircle(int r);
+	
+	public abstract void areaOfSquare(int side);
+	
+	public abstract void areaOfRectangle(int length,int width);
+	
+	public abstract void areaOfTriangle(int base,int height);
+		
+	public abstract void perimeterofCircle(int r);
+	
+	public abstract void perimeterofSquare(int side);
+	
+	public abstract void perimeterofRectangle(int length,int width);
+	
+	public abstract void perimeterofTriangle(int s1,int s2,int s3);
+	
+	public abstract void armstrongNo(int num);
+	
+	public abstract void emipNo(int num);
+	
+	public abstract void FibonacciNo(int num,int n1,int n2);
+	
+	public abstract void pronicNo(int num);
+	
+	public abstract void AutomorphicNo(int num);
+	
+	
+	
+}
