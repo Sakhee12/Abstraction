faf789f26d92b3496033f1b83677208a353588bf
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-06-19 11:02:32 +0530

    Add files via upload

---
+++ ListDriver.java
@@ -0,0 +1,13 @@
+package PrinciplesOfOops;
+
+public class ListDriver
+{
+	public static void main(String[] args) 
+	{
+		AbstractList a1 = new Arraylist();
+		a1.sort();
+		
+		AbstractList a2 = new Linkedlist();
+		a2.search();
+	}
+}
