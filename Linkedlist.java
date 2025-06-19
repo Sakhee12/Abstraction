b1dc97b36ea276de56a29dfd57d9d0c6d34cd799
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-06-19 11:01:53 +0530

    Add files via upload

---
+++ Linkedlist.java
@@ -0,0 +1,20 @@
+package PrinciplesOfOops;
+
+public class Linkedlist extends AbstractList
+{
+	public void sort()
+	{
+		System.out.println("Quick Sort");
+	}
+	
+	public void search()
+	{
+		System.out.println("Binary search");
+	}
+	
+	public void remove()
+	{
+		System.out.println("Linked Remove");
+	}
+	
+}
