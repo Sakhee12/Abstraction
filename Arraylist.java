7a9dc65732ed6cecd3a4956c018ee162a0b4b4bf
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-06-19 11:02:57 +0530

    Add files via upload

---
+++ Arraylist.java
@@ -0,0 +1,21 @@
+package PrinciplesOfOops;
+
+//import java.util.AbstractList;
+
+public class Arraylist extends AbstractList
+{
+	public void sort()
+	{
+		System.out.println("Bubble Sort");
+	}
+	
+	public void search()
+	{
+		System.out.println("Linear Search");
+	}
+	
+	public void remove()
+	{
+		System.out.println("Array Remove");
+	}
+}
